61364607132d657c6e995cdaa233e0aa67eef7ff
Troublesm1 <Jovahnn@yahoo.com>
2021-04-17 09:35:45 -0400

    BankTeller App First Build

--- src/test/java/teller/BankTest.java
+++ src/test/java/teller/BankTest.java
@@ -1,4 +1,18 @@
 package teller;
 
+import org.junit.jupiter.api.BeforeEach;
+
 public class BankTest {
+
+    Bank underTest;
+    BankAccount account1;
+    BankAccount account2;
+
+    @BeforeEach
+    public void setUp(){
+        underTest = new Bank();
+        account1 = new BankAccount("1", "Checking", 100);
+        account2 = new BankAccount("2", "Savings", 100);
+
+    }
 }
